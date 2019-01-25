d1a6ff07fa157e2ded2228685defc4cc381f979c
Pierre Jourlin <pierre.jourlin@univ-avignon.fr>
2019-01-25 16:19:01 +0100

    V0.0

---
+++ compilateur.cpp
@@ -0,0 +1,110 @@
+//  A compiler from a very simple Pascal-like structured language LL(k)
+//  to 64-bit 80x86 Assembly langage
+//  Copyright (C) 2009 Pierre Jourlin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Build with "make compilateur"
+
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+char current;				// Current car	
+
+void ReadChar(void){		// Read character and skip spaces until 
+	current=getchar();	// non space character is read
+	while(!feof(stdin) && (current==' '||current=='\t'||current=='\n'))
+	   	current=getchar();
+}
+
+void Error(string s){
+	cerr<< s << endl;
+	exit(-1);
+}
+
+// ArithmeticExpression := Term {AdditiveOperator Term}
+// Term := Digit | "(" ArithmeticExpression ")"
+// AdditiveOperator := "+" | "-"
+// Digit := "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"
+
+	
+void AdditiveOperator(void){
+	if(current=='+'||current=='-')
+		ReadChar();
+	else
+		Error("Opérateur additif attendu");	   // Additive operator expected
+}
+		
+void Digit(void){
+	if((current<'0')||(current>'9'))
+		Error("Chiffre attendu");		   // Digit expected
+	else{
+		cout << "\tpush $"<<current<<endl;
+		ReadChar();
+	}
+}
+
+void ArithmeticExpression(void);			// Called by Term() and calls Term()
+
+void Term(void){
+	if(current=='('){
+		ReadChar();
+		ArithmeticExpression();
+		if(current!=')')
+			Error("')' était attendu");		// ")" expected
+		else
+			ReadChar();
+	}
+	else 
+		if (current>='0' && current <='9')
+			Digit();
+	     	else
+			Error("'(' ou chiffre attendu");
+}
+
+void ArithmeticExpression(void){
+	char adop;
+	Term();
+	while(current=='+'||current=='-'){
+		adop=current;		// Save operator in local variable
+		AdditiveOperator();
+		Term();
+		cout << "\tpop %rbx"<<endl;	// get first operand
+		cout << "\tpop %rax"<<endl;	// get second operand
+		if(adop=='+')
+			cout << "\taddq	%rax, %rbx"<<endl;	// add both operands
+		else
+			cout << "\tsubq	%rax, %rbx"<<endl;	// substract both operands
+		cout << "\tpush %rbx"<<endl;			// store result
+	}
+
+}
+
+int main(void){	// First version : Source code on standard input and assembly code on standard output
+	ReadChar();
+	ArithmeticExpression();
+	ReadChar();
+	if(!feof(stdin))
+		cerr <<"Caractères en trop à la fin du programme : ["<<current<<"]";
+		Error("."); // unexpected characters at the end of program
+}
+		
+			
+
+
+
+
+
