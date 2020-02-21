bfdd928c689e02de79975b533da1160b65df264e
Asu <sdelang@sdelang.fr>
2020-02-21 19:50:47 +0100

    Fix static_assert from types.cpp
    Single parameter `static_assert` is a C++17 feature

--- src/types.cpp
+++ src/types.cpp
@@ -3,7 +3,6 @@
 #include <array>
 
 static constexpr std::array<const char*, 2> types{{"unsigned int", "boolean"}};
-
-static_assert(int(Type::TOTAL) == 2);
+static_assert(int(Type::TOTAL) == types.size(), "Please update `types` array when modifying the enum");
 
 const char* name(Type type) { return types[int(type)]; }
