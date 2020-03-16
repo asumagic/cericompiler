cbde73677b51a2dde184a6661fc52920ab3d063e
Asu <sdelang@sdelang.fr>
2020-03-16 20:39:00 +0100

    Remove grammar information from compiler.hpp

--- src/compiler.hpp
+++ src/compiler.hpp
@@ -48,64 +48,24 @@ class Compiler
 
 	[[nodiscard]] string_view token_text() const;
 
-	// Letter := "a"|...|"z"
 	[[nodiscard]] Type parse_identifier();
-
-	// Number := Digit{Digit}
-	// Digit := "0"|...|"9"
 	[[nodiscard]] Type parse_number();
-
-	// Factor := Number | Letter | "(" Expression ")" | "!" Factor | TypeCast
 	[[nodiscard]] Type parse_factor();
-
-	// TypeCast := Type "(" Expression ")"
 	[[nodiscard]] Type parse_type_cast();
-
-	// Term := Factor {MultiplicativeOperator Factor}
-	// MultiplicativeOperator := "*" | "/" | "%" | "&&"
 	[[nodiscard]] Type parse_term();
-
-	// SimpleExpression := Term {AdditiveOperator Term}
-	// AdditiveOperator := "+" | "-" | "||"
 	[[nodiscard]] Type parse_simple_expression();
-
-	// DeclarationPart := "VAR" VarDeclaration {";" VarDeclaration} "."
-	// VarDeclaration := Ident {"," Ident} ":" Type
-	void parse_declaration_block();
-
-	// Type := "INTEGER" | "BOOLEAN"
+	void               parse_declaration_block();
 	[[nodiscard]] Type parse_type();
-
-	// Expression := SimpleExpression [RelationalOperator SimpleExpression]
-	// RelationalOperator := "==" | "!=" | "<" | ">" | "<=" | ">="
 	[[nodiscard]] Type parse_expression();
-
-	// AssignementStatement := Identifier ":=" Expression
-	Variable parse_assignment_statement();
-
-	// IfStatement := "IF" Expression "THEN" Statement [ "ELSE" Statement ]
-	void parse_if_statement();
-
-	// WhileStatement := "WHILE" Expression DO Statement
-	void parse_while_statement();
-
-	// ForStatement := "FOR" AssignementStatement "TO" Expression "DO" Statement
-	void parse_for_statement();
-
-	// BlockStatement := "BEGIN" Statement { ";" Statement } "END"
-	void parse_block_statement();
-
-	// DisplayStatement := "DISPLAY" Expression
-	void parse_display_statement();
-
-	// Statement := AssignementStatement
-	void parse_statement();
-
-	// StatementPart := Statement {";" Statement} "."
-	void parse_statement_part();
-
-	// Program := [DeclarationPart] StatementPart
-	void parse_program();
+	Variable           parse_assignment_statement();
+	void               parse_if_statement();
+	void               parse_while_statement();
+	void               parse_for_statement();
+	void               parse_block_statement();
+	void               parse_display_statement();
+	void               parse_statement();
+	void               parse_statement_part();
+	void               parse_program();
 
 	[[noreturn]] void error(string_view s) const;
 	[[noreturn]] void bug(string_view s) const;
